65cf46b597eaa6b5954d6a6995bfac6ef2f59ea9
Harshith N J <njharshith122@gmail.com>
2025-03-12 22:21:05 +0530

    Created the project

---
+++ src/main/java/org/music_track/music_track/MusicTrackApplication.java
@@ -0,0 +1,13 @@
+package org.music_track.music_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/music_track/music_track/MusicTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.music_track.music_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
