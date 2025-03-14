64b474dcae8ce010d3b37f8b632c5dc97a3f883c
Harshith N J <njharshith122@gmail.com>
2025-03-14 18:47:00 +0530

    Add a Track Record

--- src/main/java/org/music_track/music_track/controller/trackController.java
+++ src/main/java/org/music_track/music_track/controller/trackController.java
@@ -1,7 +1,10 @@
 package org.music_track.music_track.controller;
 
+import org.music_track.music_track.dto.track;
 import org.music_track.music_track.service.trackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class trackController {
 
     @Autowired
     trackService service;
+
+    @PostMapping("/tracks")
+    public ResponseEntity<Object> createTrack(track track) {
+        return service.createTrack(track);
+    }
     
 }
--- src/main/java/org/music_track/music_track/service/trackService.java
+++ src/main/java/org/music_track/music_track/service/trackService.java
@@ -1,7 +1,13 @@
 package org.music_track.music_track.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.music_track.music_track.dto.track;
 import org.music_track.music_track.repository.trackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +15,22 @@ public class trackService {
     
     @Autowired
     trackRepository repository;
+
+    public ResponseEntity<Object> createTrack(track track) {
+        if(repository.existsByTitle(track.getTitle())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Track Already Exists with the Title" + track.getTitle());
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }else{
+            repository.save(track);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Track Created Successfully");
+            map.put("Track Details", track);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
     
 }
